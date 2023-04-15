47c291906890cfc467715bb19a7263b752dfbd3c
fengdeming <245211612@qq.com>
2023-04-15 16:18:50 +0800

    feat: 1. createElement函数 & 修改jsx插件配置为classic

--- src/index.js
+++ src/index.js
@@ -1,17 +1,48 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+/**
+ * 自定义jsx转换方法，生成 reactElement，即vnode节点结构
+ * 此方法也类似于vue的h()方法
+ */
+function createElement(type, props, ...children) {
+  return {
+    type,
+    props: {
+      ...props,
+      children: children.map((child) =>
+        typeof child === 'object' ? child : createTextElement(child)
+      ),
+    },
+  };
+}
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+function createTextElement(text) {
+  return {
+    type: 'TEXT_ELEMENT',
+    props: {
+      nodeValue: text,
+      children: [],
+    },
+  };
+}
+
+const Didact = {
+  createElement,
+};
+
+// babel-plugin-react-jsx插件提供的注释，可指定自定义jsx转换方法
+/** @jsx Didact.createElement */
+const element = (
+  <div id="foo">
+    <a id="bar" href="javascript;">
+      bar
+      <div id="bar1">bar1</div>
+      <div id="bar2">bar2</div>
+    </a>
+    <div id="baz">
+      <span id="baz1">baz1</span>
+      baz
+      <span id="baz2">baz2</span>
+    </div>
+  </div>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+console.log(element);
