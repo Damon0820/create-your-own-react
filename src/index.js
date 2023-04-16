16bca1fc45af1f988d3408627ddcecf735d1f3cd
fengdeming <245211612@qq.com>
2023-04-16 21:35:05 +0800

    feat: Fibers

--- src/index.js
+++ src/index.js
@@ -29,7 +29,7 @@ function createTextElement(text) {
  * @param {*} element ReactElement
  * @param {*} container 父元素
  */
-function render(element, container) {
+function createDom(element, container) {
   const { type, props } = element;
   let node = document.createTextNode(props.nodeValue);
   if (type !== 'TEXT_ELEMENT') {
@@ -40,11 +40,82 @@ function render(element, container) {
         node[key] = props[key];
       }
     });
-    // 递归创建子节点
-    props.children.forEach((child) => render(child, node));
   }
-  // 后序遍历，最后将整个dom树挂在到文档
-  container.appendChild(node);
+  return node;
+}
+
+/**
+ * 执行工作单元
+ * 1. 创建dom并挂在到父dom节点
+ * 2. 将 ReactElement 子元素生成 fiber 数据结构，并处理父子兄弟 fiber 节点链接关系
+ * 3. 返回下一个 fiber 作为执行单元
+ *    - 有子返回子(child)
+ *    - 若无子返回兄弟(sibling)
+ *    - 若无返回父的兄弟即叔叔（uncle）
+ */
+function performUnitOfWork(fiber) {
+  // 1
+  if (!fiber.dom) {
+    fiber.dom = createDom(fiber);
+  }
+  if (fiber.parent) {
+    fiber.parent.dom.appendChild(fiber.dom);
+  }
+  // 2
+  const elements = fiber.props.children;
+  let index = 0;
+  let prevSibling = null;
+  while (index < elements.length) {
+    const element = elements[index];
+    const newFiber = {
+      type: element.type,
+      props: element.props,
+      parent: fiber,
+      dom: null,
+    };
+    if (index === 0) {
+      fiber.child = newFiber;
+    } else {
+      prevSibling.sibling = newFiber;
+    }
+    prevSibling = newFiber;
+    index++;
+  }
+
+  // 3
+  if (fiber.child) {
+    return fiber.child;
+  }
+  let nextFiber = fiber;
+  while (nextFiber) {
+    if (nextFiber.sibling) {
+      return nextFiber.sibling;
+    }
+    nextFiber = nextFiber.parent;
+  }
+  return null;
+}
+
+let nextUnitOfWork = null;
+function workLoop(deadline) {
+  let shouldYield = false;
+  while (nextUnitOfWork && !shouldYield) {
+    nextUnitOfWork = performUnitOfWork(nextUnitOfWork);
+    shouldYield = deadline.timeRemaining() < 1;
+    console.log('nextUnitOfWork', nextUnitOfWork);
+  }
+  nextUnitOfWork && requestIdleCallback(workLoop);
+}
+
+function render(element, container) {
+  // root fiber
+  nextUnitOfWork = {
+    dom: container,
+    props: {
+      children: [element],
+    },
+  };
+  requestIdleCallback(workLoop);
 }
 
 const MyReact = {
